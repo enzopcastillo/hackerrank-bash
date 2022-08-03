3e53ed86ce2048880605a5a6f67f3e25ac0379b9
enzopcastillo <epcastillov@gmail.com>
2022-08-03 13:06:35 -0300

    Getting started with conditionals

---
+++ 7.sh
@@ -0,0 +1,13 @@
+#Read in one character from STDIN.
+#If the character is 'Y' or 'y' display "YES".
+#If the character is 'N' or 'n' display "NO".
+#No other character will be provided as input. 
+
+read palabra
+if [ $palabra == 'Y' ] || [ $palabra == 'y' ]
+    then
+        echo "YES"
+    elif [ $palabra == 'N' ] || [ $palabra == 'n' ]
+        then
+            echo "NO"
+fi
