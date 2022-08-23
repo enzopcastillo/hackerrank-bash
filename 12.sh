a6364058095497c1ee3c6a6b5da1ee5867d9fedf
Enzo Pedro Castillo <93060695+enzopcastillo@users.noreply.github.com>
2022-08-23 00:01:18 -0300

    Delete 12.sh

--- 12.sh
+++
@@ -1,4 +0,0 @@
-#Given N lines of input, print the 3rd character from each line as a new line of output. 
-#It is guaranteed that each of the n lines of input will have a 3rd character.
-
-echo "$(cut -c3)"
