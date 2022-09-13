1f967e18654aab26bd2b08c23a855ed32f2671b9
Enzo Pedro Castillo <epcastillov@gmail.com>
2022-09-13 17:30:56 -0300

    Cut #1

--- 12.sh
+++ 12.sh
@@ -1,4 +1,4 @@
 #Given N lines of input, print the 3rd character from each line as a new line of output. 
 #It is guaranteed that each of the n lines of input will have a 3rd character.
 
- echo "$(cut -c3)" 
+cut -c 3
