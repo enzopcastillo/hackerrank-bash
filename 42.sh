1eebae7d2a9bb57457709f7cc0202903c59b2ab1
Enzo Pedro Castillo <epcastillov@gmail.com>
2022-09-14 12:02:10 -0300

    Paste -1

---
+++ 42.sh
@@ -0,0 +1,4 @@
+#You are given a CSV file where each row contains the name of a city and its state separated by a comma. 
+#Your task is to replace the newlines in the file with semicolons as demonstrated in the sample.
+
+paste -s -d ';'
