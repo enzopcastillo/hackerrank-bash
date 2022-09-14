3eda5f53076093bbfb3e4714dcb65f099f8e18d8
Enzo Pedro Castillo <epcastillov@gmail.com>
2022-09-14 11:58:10 -0300

    Paste -4

---
+++ 41.sh
@@ -0,0 +1,4 @@
+#Given a CSV file where each row contains the name of a city and its state separated by a comma. 
+#Your task is to restructure the file in such a way, that three consecutive rows are folded into one, and separated by tab.
+
+paste - - -
