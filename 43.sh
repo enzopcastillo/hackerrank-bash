747749075c059907316b3d6278e769072b0dfcd1
Enzo Pedro Castillo <epcastillov@gmail.com>
2022-09-14 12:04:43 -0300

    Paste -2

---
+++ 43.sh
@@ -0,0 +1,4 @@
+#You are given a CSV file where each row contains the name of a city and its state separated by a comma. 
+#Your task is to restructure the file so that three consecutive rows are folded into one line and are separated by semicolons.
+
+paste -d ';' - - -
