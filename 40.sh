b006334feffbe134205f8ec637aabeb3cf0b1fb3
Enzo Pedro Castillo <epcastillov@gmail.com>
2022-09-14 11:50:31 -0300

    Paste -3

---
+++ 40.sh
@@ -0,0 +1,4 @@
+#Given a CSV file where each row contains the name of a city and its state separated by a comma.
+#Your task is to replace the newlines in the file with tabs as demonstrated in the sample.
+
+paste -s
